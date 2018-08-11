d321b42babd3e325ca4d1f57a21673c9b1c2812e
Roscoe Phillips <sorceo@gmail.com>
2018-08-11 11:15:46 -0400

    Added settings to the app.

--- app/src/main/java/com/example/android/rpnewsapp/MainActivity.java
+++ app/src/main/java/com/example/android/rpnewsapp/MainActivity.java
@@ -1,16 +1,19 @@
 package com.example.android.rpnewsapp;
 
 import android.app.LoaderManager;
-import android.app.LoaderManager;
 import android.app.LoaderManager.LoaderCallbacks;
 import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -18,7 +21,6 @@ import android.widget.ListView;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
@@ -27,7 +29,7 @@ public class MainActivity extends AppCompatActivity
 
     //Guardian API dataset URL.
     private static final String GUARDIAN_REQUEST_LINK =
-            "https://content.guardianapis.com/search?q=vegetables&show-fields=all&api-key=3cbb42b8-efc7-4f89-9a52-f267e2f35bd0";
+            "https://content.guardianapis.com/search?show-fields=all&api-key=3cbb42b8-efc7-4f89-9a52-f267e2f35bd0";
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
@@ -42,13 +44,27 @@ public class MainActivity extends AppCompatActivity
     private TextView noNewsText;
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //inflate options menu from XML
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Displays a toolbar programmatically.
-        Toolbar toolbar = (Toolbar) findViewById(R.id.rpnews_toolbar);
-        setSupportActionBar(toolbar);
 
         //References the ListView in the layout.
         ListView articleListView = (ListView) findViewById(R.id.list);
@@ -75,6 +91,7 @@ public class MainActivity extends AppCompatActivity
                 startActivity(webIntent);
             }
         });
+
 
         //References ConnectivityManager to check network connectivity.
         ConnectivityManager connectivityManager = (ConnectivityManager)
@@ -105,8 +122,36 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public Loader<List<NewsArticle>> onCreateLoader(int i, Bundle bundle) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        String starRating = sharedPreferences.getString(
+                getString(R.string.settings_star_rating_key),
+                getString(R.string.settings_star_rating_label));
+
+        String mediaType = sharedPreferences.getString(
+                getString(R.string.settings_media_type_key),
+                getString(R.string.settings_media_type_label));
+
+        // parse breaks apart the URI string passed into parameter
+        Uri baseUri = Uri.parse(GUARDIAN_REQUEST_LINK);
+
+        //Buildupon prepares baseUri for adding query parameters.
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        //Append query and its value.  example: star rating = 5
+        //Append media type
+        if (mediaType == getString(R.string.settings_all_value)) {
+        } else {
+            uriBuilder.appendQueryParameter("section", mediaType);
+        }
+        //Append star rating
+        uriBuilder.appendQueryParameter("star-rating", starRating);
+        uriBuilder.appendQueryParameter("order-date", "published" );
+        uriBuilder.appendQueryParameter("order-by", "newest" );
+
+
         // Create a new loader for the given URL
-        return new NewsLoader(this, GUARDIAN_REQUEST_LINK);
+        return new NewsLoader(this, uriBuilder.toString());
     }
 
     @Override
---
+++ app/src/main/java/com/example/android/rpnewsapp/SettingsActivity.java
@@ -0,0 +1,65 @@
+package com.example.android.rpnewsapp;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+    }
+
+
+    public static class NewsPreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_main);
+
+            Preference mediaType = findPreference(getString(R.string.settings_media_type_key));
+            bindPrefSummaryToValue(mediaType);
+
+            Preference starRating = findPreference(getString(R.string.settings_star_rating_key));
+            bindPrefSummaryToValue(starRating);
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object o) {
+            String stringValue = o.toString();
+            preference.setSummary(stringValue);
+            if (preference instanceof ListPreference) {
+                ListPreference listPreference = (ListPreference) preference;
+                int prefIndex = listPreference.findIndexOfValue(stringValue);
+                if (prefIndex >= 0) {
+                    CharSequence[] labels = listPreference.getEntries();
+                    preference.setSummary(labels[prefIndex]);
+                }
+            } else {
+                preference.setSummary(stringValue);
+            }
+            return true;
+        }
+
+        private void bindPrefSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String prefString = sharedPreferences.getString(preference.getKey(), "" );
+            onPreferenceChange(preference, prefString);
+        }
+
+    }
+
+}
